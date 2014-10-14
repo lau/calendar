dd99ae976c32f649691826ca7fa5c49080d1ff43
Lau Taarnskov <lau@taarnskov.com>
2014-10-14 21:23:19 -0200

    for ISO8601 a Z should be used when timezone is UTC

--- lib/kalends/formatter.ex
+++ lib/kalends/formatter.ex
@@ -10,18 +10,21 @@ defmodule Kalends.Formatter do
     "2014-09-26T17:10:20-3:00"
   """
   def iso8601(dt) do
-    total_off = total_utc_offset(dt)
-    sign = sign_for_offset8601(total_off)
-    offset_string = total_off |> secs_to_hours_mins_string
     "#{dt.year|>pad_with_zeroes(4)}-"<>
     "#{dt.month|>pad_with_zeroes(2)}-"<>
     "#{dt.date|>pad_with_zeroes(2)}"<>
     "T#{dt.hour|>pad_with_zeroes(2)}:"<>
     "#{dt.min|>pad_with_zeroes(2)}:"<>
-    "#{dt.sec|>pad_with_zeroes(2)}#{sign}"<>
-    offset_string
+    "#{dt.sec|>pad_with_zeroes(2)}"<>
+    iso8601_offset_part(total_utc_offset(dt), dt.timezone)
   end
 
+  defp iso8601_offset_part(_, time_zone) when time_zone == "UTC" or time_zone == "Etc/UTC", do: "Z"
+  defp iso8601_offset_part(total_off, time_zone) do
+    sign = sign_for_offset8601(total_off)
+    offset_amount_string = total_off |> secs_to_hours_mins_string
+    sign<>offset_amount_string
+  end
   defp total_utc_offset(dt), do: dt.utc_off + dt.std_off
   defp sign_for_offset8601(offset) when offset < 0, do: "-"
   defp sign_for_offset8601(_), do: "+"
--- test/formatter_test.exs
+++ test/formatter_test.exs
@@ -3,7 +3,7 @@ defmodule FormatterTest do
   alias Kalends.Formatter, as: Formatter
   doctest Kalends.Formatter
 
-  test "from erl" do
+  test "iso8601 formatting" do
     {:ok, time} = Kalends.DateTime.from_erl({{2014, 9, 26}, {17, 10, 20}}, "America/Montevideo")
     assert Formatter.iso8601(time) == "2014-09-26T17:10:20-3:00"
     {:ok, time} = Kalends.DateTime.from_erl({{2014, 9, 26}, {7, 0, 2}}, "Europe/Copenhagen")
@@ -12,4 +12,10 @@ defmodule FormatterTest do
     assert Formatter.iso8601(time) == "0010-09-26T07:00:02+0:50"
   end
 
+  test "iso8601 formatting with UTC time zone" do
+    {:ok, time} = Kalends.DateTime.from_erl({{2014, 9, 26}, {17, 10, 20}}, "Etc/UTC")
+    assert Formatter.iso8601(time) == "2014-09-26T17:10:20Z"
+    {:ok, time} = Kalends.DateTime.from_erl({{2014, 9, 26}, {7, 0, 2}}, "UTC")
+    assert Formatter.iso8601(time) == "2014-09-26T07:00:02Z"
+  end
 end
