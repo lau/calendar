b7e3ac2eb217ae8aafbb4bb61371ebbc59a651cc
Lau Taarnskov <lau@taarnskov.com>
2014-12-14 18:41:50 -0200

    add parsing of httpdates

--- lib/kalends/date_time/parse.ex
+++ lib/kalends/date_time/parse.ex
@@ -1,6 +1,55 @@
 defmodule Kalends.DateTime.Parse do
   alias Kalends.DateTime
 
+  @doc """
+  Parses a timestamp in RFC 2616 format.
+
+      iex> httpdate("Sat, 06 Sep 2014 09:09:08 GMT")
+      {:ok, %Kalends.DateTime{year: 2014, month: 9, day: 6, hour: 9, min: 9, sec: 8, timezone: "UTC", abbr: "UTC", std_off: 0, utc_off: 0}}
+
+      iex> httpdate("invalid")
+      {:bad_format, nil}
+
+      iex> httpdate("Foo, 06 Foo 2014 09:09:08 GMT")
+      {:error, :invalid_datetime}
+  """
+  def httpdate(rfc2616_string) do
+    ~r/(?<weekday>[^\s]{3}),\s(?<day>[\d]{2})\s(?<month>[^\s]{3})[\s](?<year>[\d]{4})[^\d](?<hour>[\d]{2})[^\d](?<min>[\d]{2})[^\d](?<sec>[\d]{2})\sGMT/
+    |> Regex.named_captures(rfc2616_string)
+    |> httpdate_parsed
+  end
+  defp httpdate_parsed(nil), do: {:bad_format, nil}
+  defp httpdate_parsed(mapped) do
+    DateTime.from_erl(
+      {
+        {mapped["year"]|>to_int,
+          mapped["month"]|>month_number_for_month_name,
+          mapped["day"]|>to_int},
+        {mapped["hour"]|>to_int, mapped["min"]|>to_int, mapped["sec"]|>to_int }
+      }, "UTC")
+  end
+  defp month_number_for_month_name(string) do
+    string
+    |> String.downcase
+    |> cap_month_number_for_month_name
+  end
+  defp cap_month_number_for_month_name("jan"), do: 1
+  defp cap_month_number_for_month_name("feb"), do: 2
+  defp cap_month_number_for_month_name("mar"), do: 3
+  defp cap_month_number_for_month_name("apr"), do: 4
+  defp cap_month_number_for_month_name("may"), do: 5
+  defp cap_month_number_for_month_name("jun"), do: 6
+  defp cap_month_number_for_month_name("jul"), do: 7
+  defp cap_month_number_for_month_name("aug"), do: 8
+  defp cap_month_number_for_month_name("sep"), do: 9
+  defp cap_month_number_for_month_name("oct"), do: 10
+  defp cap_month_number_for_month_name("nov"), do: 11
+  defp cap_month_number_for_month_name("dec"), do: 12
+  # By returning 0 for invalid month names, we have a valid int to pass to
+  # DateTime.from_erl that will return a nice error. This way we avoid an
+  # exception when parsing an httpdate with an invalid month name.
+  defp cap_month_number_for_month_name(_), do: 0
+
   @privatedoc """
   Parse RFC 3339 timestamp strings as UTC. If the timestamp is not in UTC it
   will be shifted to UTC.
