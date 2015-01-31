21583fb7c1ebdf33667dd675875e893fd684769f
Lau Taarnskov <lau@taarnskov.com>
2015-01-31 19:09:33 -0200

    Bump version. Now with tzdata 2015a.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kalends.Mixfile do
   def project do
     [app: :kalends,
      name: "Kalends",
-     version: "0.2.0-dev",
+     version: "0.2.1",
      elixir: "~> 1.0.0 or ~> 0.15.1",
      package: package,
      description: description,
