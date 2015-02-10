fd07a2e16aae2eb14b5f2a29d8795e5abf4f6c98
Lau Taarnskov <lau@taarnskov.com>
2015-02-10 11:08:31 -0200

    add specific periods_for_time functions by macros for increased performance

--- lib/kalends/tz_parsing/tz_time_zone_data_source.ex
+++ lib/kalends/tz_parsing/tz_time_zone_data_source.ex
@@ -1,32 +1,8 @@
 defmodule Kalends.TzParsing.TimeZoneDataSource do
-  # make sure TzPeriodBuilder is compiled before compiling this module
-  require Kalends.TzParsing.TzPeriodBuilder
-  require Kalends.TzParsing.TzData
-  alias Kalends.TzParsing.TzPeriodBuilder, as: TzPeriodBuilder
-  alias Kalends.TzParsing.TzData, as: TzData
+  alias Kalends.TzParsing.TzData
+  alias Kalends.TzParsing.Periods
+  alias Kalends.TzParsing.TzReleaseParser
   @moduledoc false
-
-  # For each canonical zone, calculate periods
-  Enum.each TzData.zone_list, fn (zone_name) ->
-    def periods(unquote(zone_name)) do
-      {:ok, unquote(Macro.escape(TzPeriodBuilder.calc_periods(zone_name)))}
-    end
-  end
-
-  # For each linked zone, call canonical zone
-  Enum.each TzData.links, fn {alias_name, canonical_name} ->
-    def periods(unquote(alias_name)) do
-      periods(unquote(canonical_name))
-    end
-  end
-
-  # if a periods for a zone has not been defined at this point,
-  # the zone name does not exist
-  @doc """
-  A list of pre-compiled periods for a given zone name. This function is
-  used by the TimeZonePeriods module.
-  """
-  def periods(_zone_name), do: {:error, :not_found}
 
   # Provide lists of zone- and link-names
   # Note that the function names are different from TzData!
@@ -106,7 +82,30 @@ defmodule Kalends.TzParsing.TimeZoneDataSource do
       Kalends.TimeZoneData.tzdata_version
       "2014i"
   """
-  def tzdata_version, do: unquote(Macro.escape(Kalends.TzParsing.TzReleaseParser.tzdata_version))
+  def tzdata_version, do: unquote(Macro.escape(TzReleaseParser.tzdata_version))
+
+  def periods(zone_name) do
+    Periods.periods(zone_name)
+  end
+
+  @min_cache_time_point 63555753600 # 2014
+  @max_cache_time_point 64376208000 # 2040
+  Enum.each TzData.zone_list, fn (zone_name) ->
+    {:ok, periods} = Periods.periods(zone_name)
+    Enum.each periods, fn(period) ->
+      if period.until.utc > @min_cache_time_point && period.from.utc < @max_cache_time_point do
+        def periods_for_time(unquote(zone_name), time_point, :utc) when time_point > unquote(period.from.utc) and time_point < unquote(period.until.utc) do
+          unquote(Macro.escape([period]))
+        end
+      end
+    end
+  end
+  # For each linked zone, call canonical zone
+  Enum.each TzData.links, fn {alias_name, canonical_name} ->
+    def periods_for_time(unquote(alias_name), time_point, :utc) do
+      periods_for_time(unquote(canonical_name), time_point, :utc)
+    end
+  end
 
   def periods_for_time(zone_name, time_point, time_type \\ :wall) do
     {:ok, periods} = periods(zone_name)
@@ -121,4 +120,5 @@ defmodule Kalends.TzParsing.TimeZoneDataSource do
   defp smaller_than_or_equals(first, second), do: first <= second
   defp bigger_than(:max, _), do: true
   defp bigger_than(first, second), do: first > second
+
 end
