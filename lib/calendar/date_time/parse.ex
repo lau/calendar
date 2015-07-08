268ba5447b81d493c5e3bebacbdf39567cf41edd
Lau Taarnskov <lau@taarnskov.com>
2015-07-08 16:08:08 +0200

    DateTime Parse rfc822

--- lib/calendar/date_time/parse.ex
+++ lib/calendar/date_time/parse.ex
@@ -4,6 +4,81 @@ defmodule Calendar.DateTime.Parse do
 
   @secs_between_year_0_and_unix_epoch 719528*24*3600 # From erlang calendar docs: there are 719528 days between Jan 1, 0 and Jan 1, 1970. Does not include leap seconds
 
+
+  @doc """
+  Parses an RFC 822 datetime string and shifts it to UTC.
+
+  Takes an RFC 822 `string` and `year_guessing_base`. The `year_guessing_base`
+  argument is used in case of a two digit year which is allowed in RFC 822.
+  The function tries to guess possible four digit versions of the year and
+  chooses the one closest to `year_guessing_base`. It defaults to 2015.
+
+  # Examples
+      # 2 digit year
+      iex> "5 Jul 15 20:26:13 PST" |> rfc822_utc
+      {:ok,
+            %Calendar.DateTime{abbr: "UTC", day: 6, hour: 4, min: 26, month: 7,
+             sec: 13, std_off: 0, timezone: "Etc/UTC", usec: nil, utc_off: 0,
+             year: 2015}}
+      # 82 as year
+      iex> "5 Jul 82 20:26:13 PST" |> rfc822_utc
+      {:ok,
+            %Calendar.DateTime{abbr: "UTC", day: 6, hour: 4, min: 26, month: 7,
+             sec: 13, std_off: 0, timezone: "Etc/UTC", usec: nil, utc_off: 0,
+             year: 1982}}
+      # 1982 as year
+      iex> "5 Jul 82 20:26:13 PST" |> rfc822_utc
+      {:ok,
+            %Calendar.DateTime{abbr: "UTC", day: 6, hour: 4, min: 26, month: 7,
+             sec: 13, std_off: 0, timezone: "Etc/UTC", usec: nil, utc_off: 0,
+             year: 1982}}
+      # 2 digit year and we use 2099 as the base guessing year
+      # which means that 15 should be interpreted as 2115 no 2015
+      iex> "5 Jul 15 20:26:13 PST" |> rfc822_utc(2099)
+      {:ok,
+            %Calendar.DateTime{abbr: "UTC", day: 6, hour: 4, min: 26, month: 7,
+             sec: 13, std_off: 0, timezone: "Etc/UTC", usec: nil, utc_off: 0,
+             year: 2115}}
+  """
+  def rfc822_utc(string, year_guessing_base \\ 2015) do
+    string
+    |> capture_rfc822_string
+    |> change_captured_year_to_four_digit(year_guessing_base)
+    |> rfc2822_utc_from_captured
+  end
+  defp capture_rfc822_string(string) do
+    ~r/(?<day>[\d]{1,2})[\s]+(?<month>[^\d]{3})[\s]+(?<year>[\d]{2,4})[\s]+(?<hour>[\d]{2})[^\d]?(?<min>[\d]{2})[^\d]?(?<sec>[\d]{2})[^\d]?(((?<offset_sign>[+-])(?<offset_hours>[\d]{2})(?<offset_mins>[\d]{2})|(?<offset_letters>[A-Z]{1,3})))?/
+    |> Regex.named_captures string
+  end
+  defp change_captured_year_to_four_digit(cap, year_guessing_base) do
+    changed_year = to_int(cap["year"])
+    |> two_to_four_digit_year(year_guessing_base)
+    |> Integer.to_string
+    %{cap | "year" => changed_year}
+  end
+  defp two_to_four_digit_year(year, year_guessing_base) when year < 100 do
+    closest_year(year, year_guessing_base)
+  end
+  defp two_to_four_digit_year(year, _), do: year
+
+  defp closest_year(two_digit_year, year_guessing_base) do
+    two_digit_year
+    |> possible_years(year_guessing_base)
+    |> Enum.map(fn year -> {year, abs(year_guessing_base-year)} end)
+    |> Enum.min_by(fn {year, diff} -> diff end)
+    |> elem(0)
+  end
+  defp possible_years(two_digit_year, year_guessing_base) do
+    centuries_for_guessing_base(year_guessing_base)
+    |> Enum.map(&(&1+two_digit_year))
+  end
+  # The three centuries closest to the guessing base
+  # if you provide e.g. 2015 it should return [1900, 2000, 2100]
+  defp centuries_for_guessing_base(year_guessing_base) do
+    base_century = year_guessing_base-rem(year_guessing_base, 100)
+    [base_century-100, base_century, base_century+100]
+  end
+
   @doc """
   Parses an RFC 2822 or RFC 1123 datetime string.
 
@@ -28,7 +103,12 @@ defmodule Calendar.DateTime.Parse do
              timezone: "Etc/UTC", usec: nil, utc_off: 0, year: 2010}}
   """
   def rfc2822_utc(string) do
-    cap = string |> capture_rfc2822_string
+    string
+    |> capture_rfc2822_string
+    |> rfc2822_utc_from_captured
+  end
+
+  defp rfc2822_utc_from_captured(cap) do
     month_num = month_number_for_month_name(cap["month"])
     {:ok, offset_in_secs} = offset_in_seconds_rfc2822(cap["offset_sign"],
                                                cap["offset_hours"],
