176c1d96a9b350ad5928e37965c58b98e7018842
Thijs Wouters <thijs@morewood.be>
2015-10-27 12:16:01 +0100

    Accept anything that contains time for Time.to_erl/1
    Add tests for Calendar.Time.to_erl/1, Calendar.Date.to_erl/1 and
    Calendar.DateTime.to_erl/1

--- test/date_test.exs
+++ test/date_test.exs
@@ -1,6 +1,18 @@
+defmodule SomethingThatContainsDate do
+  defstruct []
+end
+
+defimpl Calendar.ContainsDate, for: SomethingThatContainsDate do
+  def date_struct(_), do: %Calendar.Date{year: 2015, month: 1, day: 1}
+end
+
 defmodule DateTest do
   use ExUnit.Case, async: true
   import Calendar.Date
   alias Calendar.NaiveDateTime
   doctest Calendar.Date
+
+  test "to_erl works for anything that contains a date" do
+    assert to_erl(%SomethingThatContainsDate{}) == {2015, 1, 1}
+  end
 end
