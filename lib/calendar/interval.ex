da41f1082343c41f836b385f640c9a7be1cc7b57
Thijs Wouters <thijs@morewood.be>
2015-11-11 11:29:28 +0100

    Add type and spec

--- lib/calendar/interval.ex
+++ lib/calendar/interval.ex
@@ -3,12 +3,16 @@ defmodule Calendar.Interval do
 An `Interval` consists of a start and an end `DateTime`.
 """
 
+  @type t :: %__MODULE__{from: %Calendar.DateTime{}, to: %Calendar.DateTime{}}
+
   defstruct [:from, :to]
+
   use Calendar
 
   @doc """
   Returns true when the interval contains the given datetime.
   """
+  @spec includes?(t, %Calendar.DateTime{}) :: boolean
   def includes?(%Interval{from: from, to: to}, datetime) do
     DateTime.before?(from, datetime) && DateTime.after?(to, datetime)
   end
