d7141a9179572618dc8c0b1a6a14db7b5fed9294
Lau Taarnskov <lau@taarnskov.com>
2015-12-10 21:29:54 -0300

    Bump version to 0.12.0

--- mix.exs
+++ mix.exs
@@ -4,8 +4,9 @@ defmodule Calendar.Mixfile do
   def project do
     [app: :calendar,
      name: "Calendar",
-     version: "0.11.2",
-     elixir: "~> 1.1.0 or ~> 1.0.0 or ~> 1.1.0-dev",
+     version: "0.12.0",
+     elixir: "~> 1.2.0-rc.0 or 1.2.0 or ~> 1.2.0 or 1.1.0 or ~> 1.0.0",
+     consolidate_protocols: false,
      package: package,
      description: description,
      deps: deps]
