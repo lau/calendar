e564681581259d3ad3201a9ba3b14ffb12b6e477
Lau Taarnskov <lau@taarnskov.com>
2016-01-18 11:39:57 -0300

    refactor and get rid of private function in Strftime

--- lib/calendar/strftime.ex
+++ lib/calendar/strftime.ex
@@ -192,10 +192,10 @@ defmodule Calendar.Strftime do
   end
 
   defp weekday_abbr(dt, lang) do
-    Enum.fetch!(weekday_names_abbr(lang), day_of_the_week_off_by_one(dt))
+    Enum.fetch!(weekday_names_abbr(lang), day_of_the_week(dt) - 1)
   end
   defp weekday(dt, lang) do
-    Enum.fetch!(weekday_names(lang), day_of_the_week_off_by_one(dt))
+    Enum.fetch!(weekday_names(lang), day_of_the_week(dt) - 1)
   end
 
   defp month_abbr(dt, lang) do
@@ -224,12 +224,6 @@ defmodule Calendar.Strftime do
   defp x24h_to_12_h(hour) when hour >= 1 and hour < 12 do {hour, :am} end
   defp x24h_to_12_h(hour) when hour > 12 do {hour - 12, :pm} end
 
-  defp day_of_the_week_off_by_one(data) do
-    data
-    |> to_date
-    |> day_of_the_week
-    |> - 1
-  end
   defp day_of_the_week(dt) do
     dt |> Calendar.Date.day_of_week
   end
