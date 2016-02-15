0b2e5d36c2fa46a6ec71b362f6867c65765228be
Lau Taarnskov <lau@taarnskov.com>
2016-02-15 13:11:28 -0300

    Make DateTime.Interval.includes? include both from and to times as part of the interval.

--- lib/calendar/date_time/interval.ex
+++ lib/calendar/date_time/interval.ex
@@ -10,10 +10,14 @@ An `Interval` consists of a start and an end `DateTime`.
   use Calendar
 
   @doc """
-  Returns true when the interval contains the given datetime.
+  Returns `true` when the interval contains the given datetime.
+
+  "From" and "to" datetimes are treated as inclusive. This means that if the
+  provided `datetime` is between the `from` and `to` of the interval or equal
+  to either, `true` will be returned.
   """
   @spec includes?(t, %Calendar.DateTime{}) :: boolean
   def includes?(%DateTime.Interval{from: from, to: to}, datetime) do
-    DateTime.before?(from, datetime) && DateTime.after?(to, datetime)
+    !DateTime.before?(datetime, from) && !DateTime.after?(datetime, to)
   end
 end
--- test/interval_test.exs
+++ test/interval_test.exs
@@ -17,6 +17,16 @@ defmodule Calendar.DateTime.IntervalTest do
       interval, DateTime.from_erl!({{2015, 1, 1}, {13, 0, 0}}, "Europe/Brussels", 0)) == true
   end
 
+  test "includes? returns true when the interval start is equal to the the datetime", %{interval: interval} do
+    assert DateTime.Interval.includes?(
+      interval, DateTime.from_erl!({{2015, 1, 1}, {12, 0, 0}}, "Europe/Brussels", 0)) == true
+  end
+
+  test "includes? returns true when the interval end is equal to the the datetime", %{interval: interval} do
+    assert DateTime.Interval.includes?(
+      interval, DateTime.from_erl!({{2015, 1, 2}, {12, 0, 0}}, "Europe/Brussels", 0)) == true
+  end
+
   test "includes? returns false the datetime is later", %{interval: interval} do
     assert DateTime.Interval.includes?(
       interval, DateTime.from_erl!({{2015, 1, 2}, {13, 0, 0}}, "Europe/Brussels", 0)) == false
